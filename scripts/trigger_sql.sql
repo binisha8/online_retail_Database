a6f2f246af703760401294da0302a3e848ddca75
Binisha Maharjan <115027066+binisha8@users.noreply.github.com>
2025-07-22 19:29:07 +0545

    Update trigger_sql.sql

--- scripts/trigger_sql.sql
+++ scripts/trigger_sql.sql
@@ -83,3 +83,112 @@ BEGIN
 END;
 GO
 
+
+--Try to update any record from Product table
+UPDATE Products SET Price=550 WHERE ProductID=2
+
+UPDATE Products SET Price=750 WHERE ProductID=7
+
+
+SELECT * FROM Products 
+
+
+--Trigger for DELETE a record from Product Tabale
+CREATE OR ALTER TRIGGER trg_delete_Products
+ON Products
+AFTER DELETE
+AS
+BEGIN
+
+	--INSERT A RECORD INTO THE CHANGING TABLE 
+	INSERT INTO ChangeLog(TableName,Operation,RecordID,ChangeBy)
+	SELECT 'Products','DELETE',inserted.ProductID,SYSTEM_USER
+	FROM inserted;
+
+	--Display a mesage indicating thatthe trigger has failed
+	PRINT 'DELETE operation logged for Products table.';
+END;
+GO
+
+
+DELETE FROM Products WHERE ProductID=10;
+
+
+-- B. Triggers for Customers Table
+-- Trigger for INSERT on Customers table
+CREATE OR ALTER TRIGGER trg_Insert_Customers
+ON Customers
+AFTER INSERT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Insert a record into the ChangeLog Table
+	INSERT INTO ChangeLog (TableName, Operation, RecordID, ChangeBy)
+	SELECT 'Customers', 'INSERT', inserted.CustomerID, SYSTEM_USER
+	FROM inserted;
+
+	-- Display a message indicating that the trigger has fired.
+	PRINT 'INSERT operation logged for Customers table.';
+END;
+GO
+
+-- Trigger for UPDATE on Customers table
+CREATE OR ALTER TRIGGER trg_Update_Customers
+ON Customers
+AFTER UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Insert a record into the ChangeLog Table
+	INSERT INTO ChangeLog (TableName, Operation, RecordID, ChangeBy)
+	SELECT 'Customers', 'UPDATE', inserted.CustomerID, SYSTEM_USER
+	FROM inserted;
+
+	-- Display a message indicating that the trigger has fired.
+	PRINT 'UPDATE operation logged for Customers table.';
+END;
+GO
+
+-- Trigger for DELETE on Customers table
+CREATE OR ALTER TRIGGER trg_Delete_Customers
+ON Customers
+AFTER DELETE
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	-- Insert a record into the ChangeLog Table
+	INSERT INTO ChangeLog (TableName, Operation, RecordID, ChangeBy)
+	SELECT 'Customers', 'DELETE', deleted.CustomerID, SYSTEM_USER
+	FROM deleted;
+
+	-- Display a message indicating that the trigger has fired.
+	PRINT 'DELETE operation logged for Customers table.';
+END;
+GO
+
+
+--Try to insert a new record to the effect of Trigger
+INSERT INTO Customers(FirstName,LastName,Email,Phone,Address,City,State,ZipCode,Country)
+VALUES
+('Amar','Maharjan','amarmhj@gmail.com','123-456-7090','NewRoad','Kathmandu','Kathmandu',
+'4700','Nepal');
+
+--Try to update  an existing record  to use the effect of trigger
+UPDATE Customers SET State='Floride' WHERE State='IL';
+
+
+SELECT * FROM Changelog
+SELECT * FROM Customers
+SELECT * FROM Orders
+
+--Try to delete an existing record to see the effect of trigger
+DELETE FROM Customers WHERE CustomerID=4;
+ 
+
+
+
+
+
